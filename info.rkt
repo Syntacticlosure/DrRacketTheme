6ee86a2b2824f755bdaf771c788559d9cab7639c
syntacticlosure <1227847322@qq.com>
2018-07-06 09:16:51 +0800

    fix deps

--- info.rkt
+++ info.rkt
@@ -1,6 +1,7 @@
 #lang info
 (define drracket-tool-names (list "Drracket Theme"))
 (define drracket-tools (list (list "theme.rkt")))
-(define deps '("base" "drracket" "gui-lib"))
+(define deps '("base" "drracket" "gui-lib" "pict-lib" "draw-lib"))
 (define pkg-authors '(Sound!Euphonium))
 (define pkg-desc "Set the background image for DrRacket")
+(define version "0.00.20")
